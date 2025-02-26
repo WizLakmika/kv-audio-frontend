c537f03e29f79ef0464ea34aa448a39b16524096
WizLakmika <lakmikahewage28@gmail.com>
2025-02-26 12:30:31 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./index.css";
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-5xl font-bold text-red-600">
+        Tailwind CSS is Working! 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
