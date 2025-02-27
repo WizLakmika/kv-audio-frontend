3089a13a60c70b9e9bf0871518d3607028a3450d
WizLakmika <lakmikahewage28@gmail.com>
2025-02-27 23:36:19 +0530

    make frontend button using tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import "./App.css";
 import "./index.css";
-
+import { GoGraph } from "react-icons/go";
+import { FaRegBookmark } from "react-icons/fa6";
+import { MdOutlineSpeaker } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-100">
-      <h1 className="text-5xl font-bold text-red-600">
-        Tailwind CSS is Working! 🎉
-      </h1>
-    </div>
+    <div className="w-full h-screen flex">
+
+      <div className="w-[400px] h-full bg-green-400">
+
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center ">
+          <GoGraph/>
+          Dashbord
+        </button>
+
+        <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <FaRegBookmark/>
+          Booking
+          </button>
+
+          <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+            <MdOutlineSpeaker/>
+          Items
+          </button>
+
+          <button className="w-full h-[40px] text-[25px] font-bold flex justify-center items-center">
+          <FaRegBookmark/>
+          Users
+          </button>
+
+        </div>
+
+      <div className="w-full bg-red-500">
+
+    
+
+        </div> 
+
+
+      </div>
   );
 }
 
